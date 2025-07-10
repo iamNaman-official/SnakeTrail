4ef6033707e8dbd8652c7344a82721d690f873b4
iamNaman-official <namanbagrecha085@gmail.com>
2025-07-10 17:03:25 +0530

    Variables

---
+++ Python Basics/03_variable.py
@@ -0,0 +1,6 @@
+# This is variable.
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print("Hello " + name + "! You are " + age + " years old.")
+# This code demonstrates how to use variables in Python.
+# Variables are used to store data that can be used later in the program.
