9e736041c893ccd1f15dbaebf5b05af8d5c77ea0
iamNaman-official <namanbagrecha085@gmail.com>
2025-07-10 16:57:42 +0530

    Input function

---
+++ Python Basics/02_input.py
@@ -0,0 +1,4 @@
+# This code demonstrates how to use the input function in Python.
+input("Enter your name: ")
+input("Enter your age: ")
+# The input function waits for the user to type something and press Enter.
