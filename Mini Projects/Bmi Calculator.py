ac681cd32786868204462d1f660ad3b40a94d361
iamNaman-official <namanbagrecha085@gmail.com>
2025-07-10 21:05:09 +0530

    BMI Calculator

--- Mini Projects/Bmi Calculator.py
+++ Mini Projects/Bmi Calculator.py
@@ -6,3 +6,4 @@ height = float(input("Enter your height in meters: \n"))
 # The formula for calculating BMI is weight (kg) / (height (m) ** 2)
 bmi = weight / (height**2) 
 print("Your BMI is:" + str(bmi))
+
