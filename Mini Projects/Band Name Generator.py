d3b99b49186c1ade5ebe42507ab491d758944a92
iamNaman-official <namanbagrecha085@gmail.com>
2025-07-10 17:16:41 +0530

    My First Project

---
+++ Mini Projects/Band Name Generator.py
@@ -0,0 +1,9 @@
+# This is my first mini project in Python.
+# It generates a band name based on the user's input for their city and pet's name.
+print("Welcome to the Band Name Generator!")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's the name of your pet?\n")
+band_name = city + " " + pet
+print("Your band name could be: " + band_name)
+# This code takes the user's input for their city and pet's name,
+# and combines them to create a band name.
