da18f414b6a1d8b32f04da1e533fc420c169704e
iamNaman-official <namanbagrecha085@gmail.com>
2025-07-11 23:09:39 +0530

    Conditional Statement

---
+++ Python Basics/08_conditional statements.py
@@ -0,0 +1,10 @@
+# Conditional Statements in Python.
+# If and else statements.
+# Example 1: Basic if-else statement.
+print("Welcome to the Roller Coaster!")
+height= int(input("Please enter your height in cm: "))
+
+if height > 120:
+    print("You can ride the roller coaster!")
+else:
+    print("Sorry, you are not tall enough to ride the roller coaster.")
