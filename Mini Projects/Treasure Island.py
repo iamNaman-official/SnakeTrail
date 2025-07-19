a1382e81fa06312f17c204fb4ec85d05242fa299
iamNaman-official <namanbagrecha085@gmail.com>
2025-07-20 00:18:36 +0530

    wip

---
+++ Mini Projects/Treasure Island.py
@@ -0,0 +1,11 @@
+# This is my Treasure Island game code:
+print("Welcome to the Treasure Island!")
+print("Your mission is to find the treasure.")
+choice1 = input('You are at a crossroad. Where do you want to go? \n Type "Left" or "Right".').lower()
+
+if choice1 == "left":
+    print("You have chosen the left path. Continue your adventure!")
+else :    
+    print("You fell into a hole. Game Over.")
+
+
