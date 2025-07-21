a4fee81d1edd64445708c1069693b0a9cbe63acb
iamNaman-official <namanbagrecha085@gmail.com>
2025-07-21 20:58:03 +0530

    treasure island

--- Mini Projects/Treasure Island.py
+++ Mini Projects/Treasure Island.py
@@ -1,10 +1,24 @@
 # This is my Treasure Island game code:
 print("Welcome to the Treasure Island!")
 print("Your mission is to find the treasure.")
-choice1 = input('You are at a crossroad. Where do you want to go? \n Type "Left" or "Right".').lower()
-
+choice1 = input('You\'re at a crossroad. Where do you want to go? \n Type "Left" or "Right".').lower()
+# Continue the game based on the user's choice.
 if choice1 == "left":
-    print("You have chosen the left path. Continue your adventure!")
+    choice2 = input("You\'ve come to a lake. There is an island in the middle of the lake. \n Type 'Swim' to swim across or 'Wait' to wait for a boat.").lower()
+    if choice2 == "wait":
+        choice3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which color do you choose?").lower()
+        if choice3 == "red":
+            print("It's a room full of fire. Game Over.")
+        elif choice3 == "yellow":
+            print("You found the treasure! You Win!")
+        elif choice3 == "blue":
+            print("You enter a room of beasts. Game Over.")
+        else:
+            print("You chose a door that doesn't exist. Game Over.")
+    else :    
+        print("You swam across and were attacked by a trout. Game Over.")
+
+
 else :    
     print("You fell into a hole. Game Over.")
 
