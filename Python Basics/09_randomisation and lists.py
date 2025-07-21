c9503fe7cef30b59ea98c69fd67eccab6015d2ce
iamNaman-official <namanbagrecha085@gmail.com>
2025-07-21 22:46:06 +0530

    Custom module build for understanding

---
+++ Python Basics/09_randomisation and lists.py
@@ -0,0 +1,11 @@
+# # This is a simple Python script to demonstrate the use of modules and randomization.
+# # Importing the random module to generate random numbers.
+# import random
+# random_number = random.randint(1, 100)  # Generates a random number between 1 and 100.
+# print(f"Random number generated: {random_number}")
+
+
+# How to create and use a custom module.
+# This module can be used to store and retrieve a favorite number.
+import my_module  # Importing the custom module.
+print(f"My favourite number is: {my_module.my_favourite_number}")  
